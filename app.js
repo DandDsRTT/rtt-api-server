74ae48ded70e06d4afae55652f4f8b27fceb06f6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2023-01-17 10:24:18 -0800

    debugging

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ const app = express()
 
 app.use(cors())
 
+app.get("/", (req, res) => res.send("Hello world!"))
+
 app.get("*", (req, res) => {
+    console.log("you hit me")
     axios.post(
         HOST + req.url,
         {},
     ).then(data => {
+        console.log("you tried to send", data.data)
         res.send(data.data)
     }).catch(e => {}) // Wolfram works, but throws a 401 error every time. If uncaught, Render dies and doesn't respond.
 })

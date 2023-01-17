222183350741cf7dafd33bc21e4cec62be333f71
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2023-01-17 10:10:53 -0800

    no double slash; catch 401 errors, correct example endpoint

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const cors = require("cors")
 const express = require("express")
 const axios = require("axios")
 
-const HOST = "https://www.wolframcloud.com/obj/douglas.blumeyer/"
+const HOST = "https://www.wolframcloud.com/obj/douglas.blumeyer"
 
 const app = express()
 
 app.use(cors())
 
-app.get("*", async (req, res) => {
-    const {data} = await axios.post(
+app.get("*", (req, res) => {
+    axios.post(
         HOST + req.url,
         {},
-    ).catch(error => {
-        console.log("Axios post error: ", error)
-    })
-    res.send(data)
+    ).then(data => {
+        res.send(data.data)
+    }).catch() // Wolfram works, but throws a 401 error every time. If uncaught, Render dies and doesn't respond.
 })
 
 app.listen(process.env.PORT)
 
-// "/optimizeGeneratorTuningMap&unparsedT=%5B%3C1%201%200%5D%20%3C0%201%204%5D%7D&tuningSchemeSpec=TILT%20minimax-U"
+// "/optimizeGeneratorTuningMap?unparsedT=%5B%3C1%201%200%5D%20%3C0%201%204%5D%7D&tuningSchemeSpec=TILT%20minimax-U"
